96bd983648c93da2a98f3f8e7a9a867fc8b206cd
RichieeTechsHub <richieetechs@gmail.com>
2026-03-16 13:26:21 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
-const { startBot } = require("./src/bot/connect")
+const startBot = require("./src/bot/connect")
 
 console.log("🚀 Starting Lite-Ollver-MD...")
 
 startBot()
-
-// keep process alive so Heroku doesn't restart it
-setInterval(() => {}, 1000)
