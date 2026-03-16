75e0be445ef4f80acf53004f462d01f4dd15760e
RichieeTechsHub <richieetechs@gmail.com>
2026-03-16 13:58:18 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const startBot = require("./src/bot/connect");
 
 console.log("🚀 Starting Lite-Ollver-MD...");
+
 startBot().catch((err) => {
   console.error("❌ Bot start error:", err);
+  process.exit(1);
 });
