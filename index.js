5644084e4430f2e08a91c88d323e98396ce86b7b
RichieeTechsHub <richieetechs@gmail.com>
2026-03-16 19:15:32 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
-const { startServer } = require("./server");
+// index.js - Make sure it doesn't depend on server
 const connect = require("./src/core/connect");
 
-console.log("🚀 Starting Lite-Ollver-MD...");
-startServer();
-connect().catch(console.error);
+console.log("🚀 Starting Lite-Ollver-MD Worker...");
+
+connect().catch(error => {
+  console.error("❌ Fatal error:", error);
+  process.exit(1);
+});
